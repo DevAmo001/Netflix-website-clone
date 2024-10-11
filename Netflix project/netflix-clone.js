8abc52edc454ecf7039ae52137c0df1eaa8eb8ef
Amos Kipruto <kiprutoamos44@gmail.com>
2024-10-12 00:28:34 +0300

    added some functionalities using js

--- Netflix project/netflix-clone.js
+++ Netflix project/netflix-clone.js
@@ -14,8 +14,11 @@ function scrollToRight() {
   });
 }
 
-function smoothTransition() {
-  const signInButton = document.querySelector('.js-sign-in-button');
+function learnMore() {
+  const learnMore = document.querySelector('.js-learn-more');
+  const newContainer = document.querySelector('.js-new-p');
 
-  document.body.innerHTML = '';
+  newContainer.textContent = 'The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google).';
+
+  learnMore.textContent = '';
 }
